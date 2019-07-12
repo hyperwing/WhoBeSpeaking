3742c16cdb44d8d392472a61aaf96abe50676909
hyperwing <drdavidwing@gmail.com>
2019-07-11 20:23:15 -0400

    Finished

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # WhoBeSpeaking
+
+
+See who is the chattiest on your Telegram chat
+
+Dependencies:
+1. python 3
+2. Beautiful Soup
+3. Collections
+
+USAGE:
+1. download the history of the specified chat and put it into the main directory as "messages.html"
+2. run "main.py"
--- main.py
+++ main.py
@@ -1,8 +1,42 @@
 from bs4 import BeautifulSoup
+import collections
 
-with open("messages.html", "r") as f:
+
+if __name__ == "__main__":
+    participantMap ={}
     
-    contents = f.read()
+    with open("messages.html", "r") as f:
+        
+        contents = f.read()
 
-    soup = BeautifulSoup(contents, 'lxml')
-    print( soup.body.find('div', attrs={'class':'from_name'}).text)
+        soup = BeautifulSoup(contents, 'lxml')
+
+        soup =  soup.findChild("body")
+
+        soup = soup.findChild("div", {"class": "page_body chat_page"})
+        soup = soup.findChild("div", {"class": "history"})
+        children = soup.findChildren("div", {"class":"message default clearfix"})
+        
+        for message in children:
+        
+            mes = message.findChild("div", {"class":"body"})
+            name = mes.findChild("div", {"class":"from_name"}).text
+            name =name.strip()
+
+            if name not in participantMap:
+                participantMap[name] = 1 
+            else:
+                participantMap[name] = participantMap[name]+1
+
+        # for key, value in participantMap.items() :
+        #     print (key, value)
+
+        sorted_x = sorted(participantMap.items(), key=lambda kv: kv[1], reverse =True)
+
+        for i in range(0, len(sorted_x)):
+            print(str(sorted_x[i][0])+ ": " + str(sorted_x[i][1]))
+
+        print("Top poster : " +str(sorted_x[0][0]))
+        print("Least poster: " + str(sorted_x[len(sorted_x)-1][0]))
+
+
